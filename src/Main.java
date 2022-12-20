aa85e61a75885a2aea498a88abaee9cf6379105f
zabatv <germanpetrov09082009@gmail.com>
2022-12-20 20:40:12 +0300

    Цыклы.Задачи 1-4(Улучшил внешний вид кода)

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
         //С помощью цикла for выведите в консоль все числа от 1 до 10.
         for (int i = 1; i <= 10; i = i + 1) {
             System.out.println(i);
         }
         System.out.println("Следушее задание:");
+
         //С помощью цикла for выведите в консоль все числа от 10 до 1.
         for (int a = 10; a >= 1; a -= 1) {
             System.out.println(a);
         }
         System.out.println("Следушее задание:");
+
         //Выведите в консоль все четные числа от 0 до 17.
         for (int b = 0; b <= 17; b += 2) {
             System.out.println(b);
         }
         System.out.println("Следушее задание:");
+
         //Выведите в консоль все четные числа от -10 до 10.
         for (int с = -10; с <= 10; с += 1) {
-
-
             System.out.println(с);
         }
     }
